21989d803c14eca770f9e00c67ee2c207e9218ad
Paucinha <paucia.renan@gmail.com>
2025-02-03 14:43:25 -0300

    riado o arquivo ContaTerminal.java

--- ContaBanco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ ContaBanco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -4,13 +4,48 @@
 
 package org.yourcompany.yourproject;
 
-/**
- *
- * @author 55119
- */
-public class ContaTerminal {
+import java.util.Scanner;
 
+/**
+ * 
+ *
+ * @author Páucinha
+ */
+
+ public class ContaTerminal {
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Declaração das variáveis conforme os tipos indicados
+        try ( // Criar o objeto Scanner para leitura dos dados do terminal
+                Scanner scanner = new Scanner(System.in)) {
+            // Declaração das variáveis conforme os tipos indicados
+            int numeroConta;
+            String agencia;
+            String nomeCliente;
+            double saldo;
+            // Solicitar o número da conta
+            System.out.println("Por favor, digite o número da conta: ");
+            numeroConta = scanner.nextInt();
+            scanner.nextLine();  // Limpar o buffer após a leitura de um número inteiro
+            // Solicitar a agência
+            System.out.println("Por favor, digite o número da Agência: ");
+            agencia = scanner.nextLine();
+            // Solicitar o nome do cliente
+            System.out.println("Por favor, digite o nome do cliente: ");
+            nomeCliente = scanner.nextLine();
+            // Solicitar o saldo
+            System.out.println("Por favor, digite o saldo da conta: ");
+            saldo = scanner.nextDouble();
+            // Exibir as informações da conta após a coleta
+            System.out.println("\n--- Dados da Conta Bancária ---");
+            System.out.println("Número da Conta: " + numeroConta);
+            System.out.println("Agência: " + agencia);
+            System.out.println("Nome do Cliente: " + nomeCliente);
+            System.out.println("Saldo: R$ " + saldo);
+            // Fechar o objeto Scanner
+        }
     }
 }
