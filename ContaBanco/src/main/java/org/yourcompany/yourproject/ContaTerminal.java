9d3435b9e0474026a1793b6da98c87bf7cba6fb3
Paucinha <paucia.renan@gmail.com>
2025-02-03 14:11:46 -0300

    meu projeto conta banco

---
+++ ContaBanco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author 55119
+ */
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
